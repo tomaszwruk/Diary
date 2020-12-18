1a1e82944aea5fc5d7b166bddd1546d167432670
Tomasz Wruk <tomasz.wruk@kabat.pl>
2020-12-18 16:43:12 +0100

    MahApp Nug

--- Diary/AddEditStudentView.xaml.cs
+++ Diary/AddEditStudentView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,7 +18,7 @@ namespace Diary
     /// <summary>
     /// Logika interakcji dla klasy AddEditStudentView.xaml
     /// </summary>
-    public partial class AddEditStudentView : Window
+    public partial class AddEditStudentView : MetroWindow
     {
         public AddEditStudentView()
         {
