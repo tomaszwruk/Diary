518f22aeae57d62e705638b8bd82f791921c30a9
Tomasz Wruk <kontakt@daneb.pl>
2020-12-19 00:44:18 +0100

    Icommand

---
+++ Diary/Commads/RelayCommand.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Diary.Commads
+{
+    class RelayCommand : ICommand
+    {
+        readonly Action<object> _execute;
+        readonly Predicate<object> _canExecute;
+
+        public RelayCommand(Action<object> execute) : this(execute, null)
+        {
+
+        }
+
+        public RelayCommand(Action<object> execute, Predicate<object> _canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException("execute");
+            _canExecute = CanExecute;
+
+        }
+
+        
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
--- Diary/Views/AddEditStudentView.xaml.cs
+++ Diary/Views/AddEditStudentView.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace Diary
+namespace Diary.Views
 {
     /// <summary>
     /// Logika interakcji dla klasy AddEditStudentView.xaml
